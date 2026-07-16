985e1ccf9dd92c4915f9909afb8ac28373f17fde
Kingdeadface <jnarainen7@gmail.com>
2026-07-16 13:09:54 +0400

    homepage, navbar , footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('smart-budget-moris');
 }
